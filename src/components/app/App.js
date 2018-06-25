cdfbfb3cae8d7f55e421f41a7d9e43124a816bab
chetan dhatrak <ketandhatrak@gmail.com>
2018-06-26 00:22:58 +0530

    added folder structure and sign up form

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import UserSignUp from '../../components/users/sign_up';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <UserSignUp />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/users/sign_up.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, Row } from 'reactstrap';
+import '../../assets/App.css';
+
+class UserSignUp extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Row className="parent-element">
+          <div className="col-sm-12 col-md-6 col-lg-6 col-centered child-element">
+            <Form>
+              <FormGroup>
+                <Label for="user_email">Email</Label>
+                <Input type="email" name="user[email]" id="user_email" placeholder="Enter Email" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="user_password">Password</Label>
+                <Input type="password" name="user[password]" id="user_password" placeholder="Enter Password" />
+              </FormGroup>
+              <Button>Submit</Button>
+            </Form>
+            </div>          
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default UserSignUp;
